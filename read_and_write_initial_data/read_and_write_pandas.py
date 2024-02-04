1274cd7cc2ad1f2fb8edd0eb6c261ffaf0f72450
ali <aliraisi807@gmail.com>
2024-02-04 08:12:21 -0100

    Add code to the read_and_write_pandas.py

--- read_and_write_initial_data/read_and_write_pandas.py
+++ read_and_write_initial_data/read_and_write_pandas.py
@@ -1,1 +1,16 @@
 
+#first step extract the csv files from the zip folder
+#read each csv file into pandas dataframe
+#concatenate these DF into one.
+#and finally save the consolidated DF to a new csv file.
+
+import pandas as pd 
+import zipfile
+import os
+
+#print(pandas.__version__)
+directory = '/home/train/datasets/KETI'
+dataframe = {}
+dataframes_room = {}
+columns = ['co2', 'humidity', 'light', 'pir', 'temperature']
+
